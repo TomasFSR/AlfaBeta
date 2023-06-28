43f848ad9ed19ba7c807bc34b3624f509d10d6be
TomasR <noemail@rightnow.xx>
2023-06-28 20:59:13 +0200

    Cleanup

--- app/controllers/passwords_resets_controller.rb
+++ app/controllers/passwords_resets_controller.rb
@@ -7,6 +7,28 @@ class PasswordsResetsController < ApplicationController
         if @user.present?
             PasswordMailer.with(user: @user).reset.deliver_now
         end
-            redirect_to root_path, notice: "If your account exist in our database then go and check your email for further instructions"
+        redirect_to root_path, notice: "If your account exist in our database then go and check your email for further instructions"
+    end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "reset_password")
+        
+        rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to password_reset_path, alert: "Your token has expired. Please try again"
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "reset_password")
+        if  @user.update(password_params)
+            redirect_to login_path, notice: "Your password was reset successfully. You can now log in"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_16_204201) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_294201) do
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
+    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
